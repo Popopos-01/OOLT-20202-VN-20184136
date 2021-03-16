2067c32fe83a89c472699ddf75b87f957bdb23d8
HoangMaiDucLong <63653206+Popopos-01@users.noreply.github.com>
2021-03-16 16:19:31 +0700

    Update DigitalVideoDisc.java

--- lab03/src/lab03/DigitalVideoDisc.java
+++ lab03/src/lab03/DigitalVideoDisc.java
@@ -18,20 +18,35 @@ public class DigitalVideoDisc {
 	public String getTitle() {
 		return title;
 	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
 	public String getDirector() {
 		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
 	}
 
 	public String getCategory() {
 		return category;
 	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
 	public int getLength() {
 		return length;
 	}
+	public void setLength(int len) {
+		this.length = len;
+	}
 
 	public float getCost() {
 		return cost;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
 	}
 }
